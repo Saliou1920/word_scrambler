d86014a4e6e4ebe1ed83dd9dccdcf0af91c26717
Saliou1920 <diopsaliou45@gmail.com>
2021-11-12 02:37:50 -0400

    styling Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Sentence from "../Sentence/Sentence";
-
+import "./Home.css";
 export default function Home({ sentence }) {
   return (
     <div className="container">
