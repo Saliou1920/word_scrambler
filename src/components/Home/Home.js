0940a671ace947bedd435b078238b6fad0ec6443
Saliou1920 <diopsaliou45@gmail.com>
2021-11-16 03:22:51 -0400

    guessing blocks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,35 @@ import Sentence from "../Sentence/Sentence";
 import "./Home.css";
 export default function Home({ sentence }) {
   const [keydown, setKeydown] = useState();
+  const [sentenceIndex, setSentenceIndex] = useState(0);
+  const [sentenceArray, setSentenceArray] = useState(sentence.split(" "));
+
+  function Texte() {
+    return (
+      <div className="texte">
+        {sentence.split(" ").map((word, wIndex) => {
+          return (
+            <div className="texte-1">
+              {word.split("").map((letter, lIndex) => {
+                return (
+                  <input
+                    type="text"
+                    value={wIndex}
+                    maxLength="2"
+                    key={lIndex}
+                  />
+                );
+              })}
+              {/* Saliou */}
+              {word !== sentenceArray[sentenceArray.length - 1] && (
+                <input type="text" value="S" maxLength="2" className="espace" />
+              )}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 
   useKeyPress((key) => {
     console.log(key);
@@ -13,6 +42,10 @@ export default function Home({ sentence }) {
     <div className="container">
       <Sentence sentence={sentence} />
       <span className="keyboard">{keydown}</span>
+      <div>
+        <Texte />
+      </div>
+      {console.log("here:", sentence)}
     </div>
   );
 }
